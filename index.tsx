24aa9d2c1a44cd1c5e820cd3922bcb1582ce9bcd
TiIlungi <devfront0ilungui@gmail.com>
2026-03-19 14:45:46 +0100

    Security: Disable console logs in production

--- index.tsx
+++ index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Disable console logs in production for security
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
