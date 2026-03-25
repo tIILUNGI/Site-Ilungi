0980c9581f2b979ff62cde348308f277138ae283
Luciano Alfredo <lucianalfred60@gmail.com>
2026-03-25 22:07:04 +0100

    api url

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,12 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
-        port: 3000,
-        host: '0.0.0.0',
+        proxy: {
+          '/api': {
+            target: 'https://api.ilungi.digital',
+            changeOrigin: true
+          }
+        }
       },
       plugins: [react()],
       define: {
